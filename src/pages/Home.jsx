161e6af13dc64c7afe62fd7756ef8033b6aa76ae
Serega <61983829+Sergey-Sverdlov@users.noreply.github.com>
2023-11-20 02:50:21 +0300

    add react three/drei to project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import {Canvas} from "@react-three/fiber";
+import {Suspense} from "react";
+import Loader from "../components/Loader";
+// <div className="absolute top-28 left-0 right-0 z-10 flex
+//             items-center justify-center">
+//     Всплывющее окно
+// </div>
+const Home = () => (
+    <section className="w-full h-screen relative">
+        <Canvas
+            className="w-full h-screen bg-transparent"
+            camera={{near: 0.1, far: 1000}}
+        >
+            <Suspense fallback={<Loader />}>
 
-const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+            </Suspense>
+        </Canvas>
+    </section>
+);
 
 export default Home;
