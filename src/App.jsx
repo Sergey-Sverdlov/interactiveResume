8823e954e8c11fbda82ed6ebfa4b9354c4c1e054
Serega <61983829+Sergey-Sverdlov@users.noreply.github.com>
2023-11-20 02:37:39 +0300

    add path Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React from 'react';
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import {Home, About, Projects, Contact} from "../src/pages/index"
 
-const App = () => (
-    <div className="border-2 ">App 2</div>
-)
-export default App
+const App = () => {
+    return (
+        <main className="bg-slate-300/20">
+            <Router>
+                <Navbar/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/about" element={<About/>}/>
+                    <Route path="/projects" element={<Projects />}/>
+                    <Route path="/contact" element={<Contact/>}/>
+                </Routes>
+            </Router>
+        </main>
+    );
+};
+
+export default App;
