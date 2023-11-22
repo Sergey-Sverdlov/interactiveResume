0261e0170adfdb55daea2102072954ecf9414ac3
Serega <61983829+Sergey-Sverdlov@users.noreply.github.com>
2023-11-22 17:00:02 +0300

    add sky 3d Model

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -6,58 +6,47 @@ Source: https://sketchfab.com/3d-models/foxs-islands-163b68e09fcc47618450150be77
 Title: Fox's islands
 */
 
-import React, { useRef, useEffect } from "react";
-import { useGLTF } from "@react-three/drei";
-import {useThree, useFrame } from "@react-three/fiber";
+import React, {useRef, useEffect} from "react";
+import {useGLTF} from "@react-three/drei";
+import {useThree, useFrame} from "@react-three/fiber";
+import islandScene from "../assets/3d/island.glb"
+import {a} from "@react-spring/three"
 
 const Island = (props) => {
-        const { nodes, materials } = useGLTF("/island.glb");
-        return (
-            <group {...props} dispose={null}>
-                    <mesh
-                        castShadow
-                        receiveShadow
-                        geometry={nodes.polySurface944_tree_body_0.geometry}
-                        material={materials.PaletteMaterial001}
-                    />
-                    <mesh
-                        castShadow
-                        receiveShadow
-                        geometry={nodes.polySurface945_tree1_0.geometry}
-                        material={materials.PaletteMaterial001}
-                    />
-                    <mesh
-                        castShadow
-                        receiveShadow
-                        geometry={nodes.polySurface946_tree2_0.geometry}
-                        material={materials.PaletteMaterial001}
-                    />
-                    <mesh
-                        castShadow
-                        receiveShadow
-                        geometry={nodes.polySurface947_tree1_0.geometry}
-                        material={materials.PaletteMaterial001}
-                    />
-                    <mesh
-                        castShadow
-                        receiveShadow
-                        geometry={nodes.polySurface948_tree_body_0.geometry}
-                        material={materials.PaletteMaterial001}
-                    />
-                    <mesh
-                        castShadow
-                        receiveShadow
-                        geometry={nodes.polySurface949_tree_body_0.geometry}
-                        material={materials.PaletteMaterial001}
-                    />
-                    <mesh
-                        castShadow
-                        receiveShadow
-                        geometry={nodes.pCube11_rocks1_0.geometry}
-                        material={materials.PaletteMaterial001}
-                    />
-            </group>
-        );
+    const islandRef = useRef();
+    const {nodes, materials} = useGLTF(islandScene);
+    return (
+        <a.group {...props} ref={islandRef}>
+            <mesh
+                geometry={nodes.polySurface944_tree_body_0.geometry}
+                material={materials.PaletteMaterial001}
+            />
+            <mesh
+                geometry={nodes.polySurface945_tree1_0.geometry}
+                material={materials.PaletteMaterial001}
+            />
+            <mesh
+                geometry={nodes.polySurface946_tree2_0.geometry}
+                material={materials.PaletteMaterial001}
+            />
+            <mesh
+                geometry={nodes.polySurface947_tree1_0.geometry}
+                material={materials.PaletteMaterial001}
+            />
+            <mesh
+                geometry={nodes.polySurface948_tree_body_0.geometry}
+                material={materials.PaletteMaterial001}
+            />
+            <mesh
+                geometry={nodes.polySurface949_tree_body_0.geometry}
+                material={materials.PaletteMaterial001}
+            />
+            <mesh
+                geometry={nodes.pCube11_rocks1_0.geometry}
+                material={materials.PaletteMaterial001}
+            />
+        </a.group>
+    );
 }
 
-useGLTF.preload("/island.glb");
+export default Island
