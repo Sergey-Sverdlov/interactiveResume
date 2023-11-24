f4357a2f0620277597f55e335f35a9084fd81167
Serega <61983829+Sergey-Sverdlov@users.noreply.github.com>
2023-11-24 03:31:03 +0300

    add model plane and rotating

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -12,8 +12,88 @@ import {useThree, useFrame} from "@react-three/fiber";
 import islandScene from "../assets/3d/island.glb"
 import {a} from "@react-spring/three"
 
-const Island = (props) => {
+const Island = ({isRotating, setIsRotating, ...props}) => {
     const islandRef = useRef();
+    const {gk, viewport} = useThree()
+
+    const lastX = useRef(0)
+    const rotationSpeed = useRef(0)
+    const dampingFactor = 0.95;
+
+    const handlePointerDown = (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        setIsRotating(true)
+
+        const clientX = e.touches
+            ? e.touches[0].clientX
+            : e.clientX
+
+        lastX.current = clientX
+    }
+
+    const handlePointerUp = (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        setIsRotating(false)
+
+        const clientX = e.touches
+            ? e.touches[0].clientX
+            : e.clientX
+
+        const delta = (clientX - lastX.current) / viewport.width;
+
+        islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+        lastX.current = clientX;
+        rotationSpeed.current = delta * 0.01 * Math.PI
+
+    }
+
+    const handlePointerMove = (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        if (isRotating) {
+            handlePointerUp(e)
+        }
+    }
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'ArrowLeft') {
+            if (!isRotating) setIsRotating(true)
+            islandRef.current.rotation.y += 0.01 * Math.PI
+        } else if (e.key === 'ArrowRight') {
+            if (!isRotating) setIsRotating(true)
+            islandRef.current.rotation.y -= 0.01 * Math.PI
+        }
+    }
+
+    const handleKeyUp = (e) => {
+        if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') setIsRotating(false)
+    }
+
+    useFrame(() => {
+        if (!isRotating) {
+            rotationSpeed.current *= dampingFactor;
+            if (Math.abs(rotationSpeed.current) < 0.01) {
+                rotationSpeed.current = 0
+            }
+        }
+    })
+
+    useEffect(() => {
+        document.addEventListener('pointerdown', handlePointerDown);
+        document.addEventListener('pointerup', handlePointerUp);
+        document.addEventListener('pointermove', handlePointerMove);
+        document.addEventListener('keydown', handleKeyDown);
+        document.addEventListener('keyup', handleKeyUp);
+        return () => {
+            document.removeEventListener('pointerdown', handlePointerDown);
+            document.removeEventListener('pointerup', handlePointerUp);
+            document.removeEventListener('pointermove', handlePointerMove);
+            document.removeEventListener('keydown', handleKeyDown);
+            document.removeEventListener('keyup', handleKeyUp);
+        }
+    }, [gl, handlePointerDown, handlePointerUp, handlePointerMove])
     const {nodes, materials} = useGLTF(islandScene);
     return (
         <a.group {...props} ref={islandRef}>
