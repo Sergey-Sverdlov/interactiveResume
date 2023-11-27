2f1d319d60f0956f36381b5ad3ac62d64370859c
Serega <61983829+Sergey-Sverdlov@users.noreply.github.com>
2023-11-28 02:24:57 +0300

    add information about job

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import {skills} from "../constants";
+import {skills, experiences} from "../constants";
+
+import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 
 const About = () => {
     return (
@@ -22,7 +25,7 @@ const About = () => {
                     <div className="mt-16 flex flex-wrap gap-16">
                         {skills.map((skill) => (
                             <div className="block-container w-28 h-28">
-                                <div className="btn-back rounded-x1" />
+                                <div className="btn-back rounded-x1"/>
                                 <div className="btn-front rounded-x1 flex flex-col  justify-center items-center">
                                     <img
                                         src={skill.imageUrl}
@@ -38,7 +41,68 @@ const About = () => {
                         ))}
                     </div>
                 </div>
+
+                <div className="py-16">
+                    <h3 className="subhead-text">Стаж работы</h3>
+                    <div className="mt-5 flex flex-col gap-3 text-slate-500 ">
+                        <p className="break-all border-2 h-[300px] break-keep break-words">
+                            У меня есть многолетний опыт в проектировании, разработки и внедрении WEB-технологий
+                            в самые разные проекты.
+                            Большое количество собственных и коммерческих проектов с использованием самых современных
+                            WEB-3 технологий.
+                            Являюсь выпускником университета им. Баумана по направлению "Программная инженерия"
+                            Более пяти лет являюсь преподавателем программирования для людей со всего мира!
+                            Люблю узнавать что-то новое, рассказывать и помогать с web-программированием.
+                        </p>
+                    </div>
+                    <div className="nt-12 flex">
+                        <VerticalTimeline>
+                            {experiences.map((experience) => (
+                                <VerticalTimelineElement
+                                    key={experience.company_name}
+                                    date={experience.date}
+                                    icon={
+                                        <div className="flex justify-center items-center w-full h-full">
+                                            <img
+                                                src={experience.icon}
+                                                alt={experience.company_name}
+                                                className="w-[60%] h-[60%] object-contain"
+                                            />
+                                        </div>}
+                                    iconStyle = {{ background: experience.iconBg }}
+                                    contentStyle={{
+                                        borderBottom: '8px',
+                                        borderStyle: 'solid',
+                                        borderBottomColor: experience.iconBg,
+                                        boxShadow: 'none'
+                                    }
+                                    }>
+                                    <div>
+                                        <h3 className="text-black text-xl
+                                        font-poppis font-semibold">
+                                            {experience.title}
+                                        </h3>
+                                        <p className="text-black-500 font-medium
+                                        font-base" style={{margin: 0}}>
+                                            {experience.company_name}
+                                        </p>
+                                    </div>
+                                    <ul className="my-5 list-disc ml-5 space-y-2">
+                                        {experience.points.map((point, index) => (
+                                            <li key={`experience-point-${index}`} className="text-black-500/50 font-normal pl-1
+                                            text-sm">
+                                                {point}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </VerticalTimelineElement>
+                            ))}
+                        </VerticalTimeline>
+                    </div>
+                </div>
             </div>
+            <hr className="border-slate-200" />
+
         </section>
     );
 };
