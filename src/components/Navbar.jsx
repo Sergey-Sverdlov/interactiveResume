195f68a23692194e24309531030099e2801753ba
Serega <61983829+Sergey-Sverdlov@users.noreply.github.com>
2024-02-08 03:26:51 +0300

    change font size on main page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,14 +7,14 @@ const Navbar = () => {
             <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
                 <p className="blue-gradient_text">SS</p>
             </NavLink>
-            <nav className="flex text-lg gap-12 font-medium max-[400px]:flex-col ">
-                <NavLink to="/about" className={({isActive}) => !isActive ? 'text-blue-500' : 'text-black'}>
+            <nav className="flex text-2xl gap-12 font-medium max-[400px]:flex-col ">
+                <NavLink to="/about" className={({isActive}) => !isActive ? 'text-blue-900' : 'text-black'}>
                     <p>Информация</p>
                 </NavLink>
-                <NavLink to="/projects" className={({isActive}) => !isActive ? 'text-blue-500' : 'text-black'}>
+                <NavLink to="/projects" className={({isActive}) => !isActive ? 'text-blue-900 ' : 'text-black'}>
                     <p>Проекты</p>
                 </NavLink>
-                <NavLink to="/contact" className={({isActive}) => !isActive ? 'text-blue-500' : 'text-black'}>
+                <NavLink to="/contact" className={({isActive}) => !isActive ? 'text-blue-900 ' : 'text-black'}>
                     <p>Связаться со мной</p>
                 </NavLink>
             </nav>
